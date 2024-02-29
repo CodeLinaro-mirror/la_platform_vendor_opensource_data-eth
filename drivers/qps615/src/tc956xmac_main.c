324f27818cfe4578ad9cbd1a4b2b70ac09a785e7
Andre Ding <quic_shuangxi@quicinc.com>
2024-02-29 14:34:37 +0530

    driver: ethernet: Fix compiling issue for QPS615 driver
    Build error with flavor "-Werror".
    
    Signed-off-by: Andre Ding <quic_shuangxi@quicinc.com>
    Change-Id: I301fd24aaed4b35a140908ed14126aeba135d821

--- drivers/qps615/src/tc956xmac_main.c
+++ drivers/qps615/src/tc956xmac_main.c
@@ -8297,7 +8297,8 @@ static netdev_tx_t tc956xmac_xmit(struct sk_buff *skb, struct net_device *dev)
 		if (skb->data[19] & 0x01) {
 			struct timespec64 ts = ns_to_timespec64(skb->tstamp);
 			u32 Presentation_time, Traverse_time, app_launch_time;
-			u64 ns, lt;
+			u64 ns = 0;
+			u64 lt;
 #ifndef CONFIG_ARCH_DMA_ADDR_T_64BIT
 			u64 quotient;
 			u32 reminder;
@@ -9961,9 +9962,9 @@ static int tc956xmac_ioctl_set_est(struct tc956xmac_priv *priv, void __user *dat
 	struct tc956xmac_est *cfg = priv->plat->est;
 	struct tc956xmac_ioctl_est_cfg *est;
 	int ret = 0;
-	u64 system_time;
+	u64 system_time = 0;
 	u32 system_time_s;
-	u32 system_time_ns;
+	u32 system_time_ns = 0;
 #ifndef CONFIG_ARCH_DMA_ADDR_T_64BIT
 	u64 quotient;
 	u32 reminder;
@@ -10630,7 +10631,8 @@ static int tc956x_xgmac_get_fw_status(struct tc956xmac_priv *priv,
 static void tc956x_ptp_configuration(struct tc956xmac_priv *priv, u32 tcr_config)
 {
 	struct timespec64 now;
-	u32 control, sec_inc;
+	u32 control;
+	u32 sec_inc = 0;
 	u64 temp;
 
 	if (tcr_config == 0) {
@@ -10878,7 +10880,7 @@ static int tc956xmac_ptp_clk_config(struct tc956xmac_priv *priv, void __user *da
 	int ret = 0;
 	u32 value = 0;
 	__u64 temp = 0;
-	__u32 sec_inc;
+	__u32 sec_inc = 0;
 	struct timespec64 now;
 
 	DBGPR_FUNC(priv->device, "--> %s\n", __func__);
