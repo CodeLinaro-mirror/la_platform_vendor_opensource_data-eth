7b4c48fbfd90f8681af7110d2da67083b156ca65
Andre Ding <quic_shuangxi@quicinc.com>
2024-03-07 14:17:29 +0530

    Driver: ethernet: Add compiling path at dlkm
    Add the build path for Qps615 driver
    
    Change-Id: Ic2f5b8898b5fb9726be886c1bf84175e0568695f
    Signed-off-by: Andre Ding <quic_shuangxi@quicinc.com>

--- dataeth_dlkm_vendor_board.mk
+++ dataeth_dlkm_vendor_board.mk
@@ -1,2 +1,8 @@
+ifneq (,$(call is-board-platform-in-list2,gen4))
 BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/emac_ctrl_fe_virtio.ko
 BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/stmmac.ko
+endif
+
+ifneq (,$(call is-board-platform-in-list2,pineapple))
+BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/tc956x_pcie_eth.ko
+endif
--- dataeth_dlkm_vendor_product.mk
+++ dataeth_dlkm_vendor_product.mk
@@ -1,3 +1,9 @@
 # Build DATA-ETH kernel drivers
+ifneq (,$(call is-board-platform-in-list2,gen4))
 PRODUCT_PACKAGES += $(KERNEL_MODULES_OUT)/stmmac.ko\
-	$(KERNEL_MODULES_OUT)/emac_ctrl_fe_virtio.ko \
+	$(KERNEL_MODULES_OUT)/emac_ctrl_fe_virtio.ko
+endif
+
+ifneq (,$(call is-board-platform-in-list2,pineapple))
+PRODUCT_PACKAGES += $(KERNEL_MODULES_OUT)/tc956x_pcie_eth.ko
+endif
