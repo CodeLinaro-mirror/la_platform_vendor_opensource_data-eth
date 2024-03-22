8a03d43a449c2f2d17c7a352e335e6fd9e3a1fc7
Srinath Pandey <quic_srinpand@quicinc.com>
2024-03-22 22:50:48 +0530

    drivers: ethernet: Added config based check
    Added config based check for the compilation
    of the emac_shim and emac_ctrl_fe_virtio driver.
    
    Change-Id: I7ddc1cbb513bd8dbb19c9cc1ebc17a94fa728d2f
    Signed-off-by: Srinath Pandey <quic_srinpand@quicinc.com>

--- dataeth_dlkm_vendor_board.mk
+++ dataeth_dlkm_vendor_board.mk
@@ -1,7 +1,5 @@
-ifneq (,$(call is-board-platform-in-list2,gen4))
 BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/emac_ctrl_fe_virtio.ko
 BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/stmmac.ko
-endif
 
 ifneq (,$(call is-board-platform-in-list2,pineapple))
 BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/tc956x_pcie_eth.ko
