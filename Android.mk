18826d81b74d5c564d228684076898f848e6839b
Srinath Pandey <quic_srinpand@quicinc.com>
2024-04-22 23:42:46 +0530

    drivers: ethernet: Follow Makefile rules
    Follow the predefined rules for writing
    Android and other makefiles.
    
    Change-Id: Id3c39afb06b6c8fa4e26f710657abb991f5a0477
    Signed-off-by: Srinath Pandey <quic_srinpand@quicinc.com>

--- Android.mk
+++ Android.mk
@@ -44,7 +44,7 @@ include $(DLKM_DIR)/Build_external_kernelmodule.mk
 # as well if corresponding flags are added in KBUILD_OPTIONS from upper
 # level Makefiles.
 
-ifeq (gen4_gvm,$(filter $(TARGET_PRODUCT), gen4_gvm))
+ifeq ($(TARGET_BOARD_PLATFORM)$(TARGET_BOARD_SUFFIX)$(TARGET_BOARD_DERIVATIVE_SUFFIX), gen4_gvm)
 ########################## emac_ctrl_fe_virtio ############################
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES           := $(DATAETH_SRC_FILES)
