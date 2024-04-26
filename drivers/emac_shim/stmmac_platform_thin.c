870ebd847df521bd4c5fc726b12fd93253077b16
Shashank Shekhar <quic_sshe@quicinc.com>
2024-04-26 17:17:24 +0530

    drivers: emac_shim: Handle interrupt for Rx and Tx discreetly
    Added support for per-channel interrupt and resolve S2R issue.
    
    Change-Id: Ib6b2e86250509e2b087ce2f42ebe01d3edb4a51e
    Signed-off-by: Shashank Shekhar <quic_sshe@quicinc.com>

--- drivers/emac_shim/stmmac_platform_thin.c
+++ drivers/emac_shim/stmmac_platform_thin.c
@@ -264,19 +264,35 @@ int stmmac_get_platform_resources(struct platform_device *pdev,
 	/* Get IRQ information early to have an ability to ask for deferred
 	 * probe if needed before we went too far with resource allocation.
 	 */
-	stmmac_res->irq[0] = platform_get_irq_byname(pdev, "tx_rx_ch0_intr");
-	stmmac_res->irq[1] = platform_get_irq_byname(pdev, "tx_rx_ch1_intr");
-	stmmac_res->irq[2] = platform_get_irq_byname(pdev, "tx_rx_ch2_intr");
-	stmmac_res->irq[3] = platform_get_irq_byname(pdev, "tx_rx_ch3_intr");
-	stmmac_res->irq[4] = platform_get_irq_byname(pdev, "tx_rx_ch4_intr");
-	stmmac_res->irq[5] = platform_get_irq_byname(pdev, "tx_rx_ch5_intr");
-	stmmac_res->irq[6] = platform_get_irq_byname(pdev, "tx_rx_ch6_intr");
-	stmmac_res->irq[7] = platform_get_irq_byname(pdev, "tx_rx_ch7_intr");
+	stmmac_res->tx_irq[0] = platform_get_irq_byname(pdev, "tx_ch0_intr");
+	stmmac_res->tx_irq[1] = platform_get_irq_byname(pdev, "tx_ch1_intr");
+	stmmac_res->tx_irq[2] = platform_get_irq_byname(pdev, "tx_ch2_intr");
+	stmmac_res->tx_irq[3] = platform_get_irq_byname(pdev, "tx_ch3_intr");
+	stmmac_res->tx_irq[4] = platform_get_irq_byname(pdev, "tx_ch4_intr");
+	stmmac_res->tx_irq[5] = platform_get_irq_byname(pdev, "tx_ch5_intr");
+	stmmac_res->tx_irq[6] = platform_get_irq_byname(pdev, "tx_ch6_intr");
+	stmmac_res->tx_irq[7] = platform_get_irq_byname(pdev, "tx_ch7_intr");
+	stmmac_res->rx_irq[0] = platform_get_irq_byname(pdev, "rx_ch0_intr");
+	stmmac_res->rx_irq[1] = platform_get_irq_byname(pdev, "rx_ch1_intr");
+	stmmac_res->rx_irq[2] = platform_get_irq_byname(pdev, "rx_ch2_intr");
+	stmmac_res->rx_irq[3] = platform_get_irq_byname(pdev, "rx_ch3_intr");
+	stmmac_res->rx_irq[4] = platform_get_irq_byname(pdev, "rx_ch4_intr");
+	stmmac_res->rx_irq[5] = platform_get_irq_byname(pdev, "rx_ch5_intr");
+	stmmac_res->rx_irq[6] = platform_get_irq_byname(pdev, "rx_ch6_intr");
+	stmmac_res->rx_irq[7] = platform_get_irq_byname(pdev, "rx_ch7_intr");
+
 	for (i = 0; i < MAX_NUM_CH; i++) {
-		if (stmmac_res->irq[i] < 0) {
+		if (stmmac_res->tx_irq[i] < 0) {
 			dev_warn(&pdev->dev,
-				 "ch irq [%d] not configured\n", i);
-			stmmac_res->irq[i] = 0;
+				 "tx ch irq [%d] not configured\n", i);
+			stmmac_res->tx_irq[i] = 0;
+		}
+	}
+	for (i = 0; i < MAX_NUM_CH; i++) {
+		if (stmmac_res->rx_irq[i] < 0) {
+			dev_warn(&pdev->dev,
+				 "rx ch irq [%d] not configured\n", i);
+			stmmac_res->rx_irq[i] = 0;
 		}
 	}
 
