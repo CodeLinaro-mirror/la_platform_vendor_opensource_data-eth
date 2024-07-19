0c407c265c8991e49eba288c916e46d77f58bf7f
Vatte Vishnuvardhan Reddy <quic_vishnuvr@quicinc.com>
2024-07-19 11:49:24 +0530

    data-eth: emac-ioss: Clear tx queue to pcp map on clear_qos
    Clear TX queue to PCP map when qos tables are cleared.
    
    Change-Id: I694a203055bde94ba7b9241385032ffa6ca372d8
    Signed-off-by: Vatte Vishnuvardhan Reddy <quic_vishnuvr@quicinc.com>
    (cherry picked from commit 8bef51d9e344012690f558270eec013a2762cd23)

--- drivers/emac_ioss/emac_ioss.c
+++ drivers/emac_ioss/emac_ioss.c
@@ -2132,6 +2132,7 @@ static int stmmac_clear_qos(struct ioss_device *idev)
 	struct net_device *ndev = idev->net_dev;
 	struct stmmac_priv *priv = netdev_priv(ndev);
 	struct dma_filter_table *dma_filter_node;
+	int i = 0;
 
 	ioss_qos_dev_log(idev, "Enter");
 
@@ -2160,6 +2161,10 @@ static int stmmac_clear_qos(struct ioss_device *idev)
 
 		memset(&qos_tables, 0, sizeof(struct qos_struct));
 	}
+
+	for (i = 0; i < priv->plat->tx_queues_to_use; i++)
+		priv->tx_queue_pcp_map[i] = 0;
+
 	return 0;
 }
 
