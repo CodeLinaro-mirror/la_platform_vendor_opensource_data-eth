bb5311a9e2a14f05ae09cad3064147ac60e005c8
Mohd Ayaan Anwar <quic_mohdayaa@quicinc.com>
2024-10-22 20:07:44 +0530

    drivers: ioss: Resolve incorrect IPA reconnect skip for Rx HW TCs
    The QOS logic tries to avoid IPA reconnects as much as possible.
    However, there is a special case in which a HW Rx QOS channels changes
    to a SW one after a commit. Add this as an extra condition when trying
    to check whether an IPA reconnect is needed or not.
    
    Change-Id: If521643e12758701c7d5683343a4558e705decde
    Signed-off-by: Mohd Ayaan Anwar <quic_mohdayaa@quicinc.com>

--- drivers/ioss/ioss_qos.c
+++ drivers/ioss/ioss_qos.c
@@ -2332,6 +2332,10 @@ static bool ioss_ipa_reconnect_required(struct ioss_device *idev, struct respons
 		ioss_qos_dev_log(idev, "ch_rx = %d, is_sw = %d -> %d, bmap = %u -> %u \n",
 			i, idev->curr_qos_config.is_rx_tc_sw[i], resp->qos_pipe_mapping.is_rx_tc_sw[i],
 			idev->curr_qos_config.pipe_to_tc_mapping_rx[i], resp->qos_pipe_mapping.pipe_to_tc_mapping_rx[i]);
+
+		/* Special Case: HW channel becomes a SW channel and vice-versa */
+		if (resp->qos_pipe_mapping.is_rx_tc_sw[i] != idev->curr_qos_config.is_rx_tc_sw[i])
+			return true;
 		if (resp->qos_pipe_mapping.is_rx_tc_sw[i])
 			continue;
 		if (resp->qos_pipe_mapping.pipe_to_tc_mapping_rx[i] != idev->curr_qos_config.pipe_to_tc_mapping_rx[i])
