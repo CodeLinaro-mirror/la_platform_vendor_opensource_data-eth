44eb39a92b03c8f0b72f856c67d0713afa62318a
Srinath Pandey <quic_srinpand@quicinc.com>
2024-12-16 17:47:08 +0530

    data-eth: emac_shim: Use spin_lock for irq thread
    Mutex locks invoke might_sleep in irq thread which is
    not appreciated and causes kernel panic in race
    in high CPU case.
    
    Change-Id: I4940a329f322a8d4e4b6afc5125f56fff3929daa
    Signed-off-by: Srinath Pandey <quic_srinpand@quicinc.com>

--- drivers/emac_shim/dwmac-qcom-ethqos-thin.c
+++ drivers/emac_shim/dwmac-qcom-ethqos-thin.c
@@ -47,12 +47,13 @@ static void emac_fe_ev_wq(struct work_struct *work)
 
 	ETHQOSINFO("Enter - cur state [%u]\n", priv->emac_state);
 	do {
-		mutex_lock(&ethqos->lock);
+		unsigned long flags;
+		spin_lock_irqsave(&ethqos->lock, flags);
 		emac_ev = list_first_entry_or_null(&ethqos->emac_fe_ev_q,
 						   struct emac_fe_ev, list);
 		if (emac_ev)
 			list_del(&emac_ev->list);
-		mutex_unlock(&ethqos->lock);
+		spin_unlock_irqrestore(&ethqos->lock, flags);
 
 		if (!emac_ev)
 			break;
@@ -123,6 +124,7 @@ static int qcom_ethqos_emac_notify_cb(struct notifier_block *nb,
 	struct qcom_ethqos *ethqos = container_of(nb, struct qcom_ethqos,
 						  emac_nb);
 	struct emac_fe_ev *emac_ev;
+	unsigned long flags;
 
 	ETHQOSINFO("event [%d]\n", ev);
 
@@ -134,9 +136,10 @@ static int qcom_ethqos_emac_notify_cb(struct notifier_block *nb,
 		return -ENOMEM;
 
 	emac_ev->ev = ev;
-	mutex_lock(&ethqos->lock);
+
+	spin_lock_irqsave(&ethqos->lock, flags);
 	list_add_tail(&emac_ev->list, &ethqos->emac_fe_ev_q);
-	mutex_unlock(&ethqos->lock);
+	spin_unlock_irqrestore(&ethqos->lock, flags);
 
 	queue_work(ethqos->wq, &ethqos->emac_fe_work);
 
@@ -541,7 +544,7 @@ static int qcom_ethqos_probe(struct platform_device *pdev)
 		goto err_smmu;
 	}
 
-	mutex_init(&ethqos->lock);
+	spin_lock_init(&ethqos->lock);
 	INIT_WORK(&ethqos->emac_fe_rdy_work, ethqos_emac_fe_ready_wq);
 	INIT_WORK((struct work_struct *)&ethqos->emac_fe_work, emac_fe_ev_wq);
 	INIT_LIST_HEAD(&ethqos->emac_fe_ev_q);
@@ -580,7 +583,6 @@ static int qcom_ethqos_probe(struct platform_device *pdev)
 
 err_reg:
 	destroy_workqueue(ethqos->wq);
-	mutex_destroy(&ethqos->lock);
 err_smmu:
 	of_platform_depopulate(&pdev->dev);
 	return ret;
@@ -604,7 +606,6 @@ static int qcom_ethqos_remove(struct platform_device *pdev)
 	ETHQOSINFO("Enter\n");
 	qcom_ethqos_unregister_emac_fe_listener(ethqos, EMAC_DMA_DRV_UNMOUNT);
 	destroy_workqueue(ethqos->wq);
-	mutex_destroy(&ethqos->lock);
 	ret = stmmac_thin_pltfr_remove(pdev);
 
 	emac_emb_smmu_exit();
