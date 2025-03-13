b9c13ce0ca7a508d3fced4bfd086974e749b6099
Shashank Shekhar <quic_sshe@quicinc.com>
2025-03-13 20:36:10 +0530

    dataeth: emac_shim: Remove mac filter
    Remove mac and broadcast filter while
    suspend and release.
    
    Change-Id: I6c9c5a9f7784b78af8297d653c21075591f54e43
    Signed-off-by: Shashank Shekhar <quic_sshe@quicinc.com>

--- drivers/emac_shim/stmmac_thin.c
+++ drivers/emac_shim/stmmac_thin.c
@@ -1308,10 +1308,19 @@ static int stmmac_release(struct net_device *dev)
 {
 	struct stmmac_priv *priv = netdev_priv(dev);
 	u32 ch = priv->queue;
+	int filter_del;
 
 	dev_info(priv->device, "%s Enter\n", __func__);
 	priv->dev_inited = false;
 	priv->dev_opened = false;
+
+	if (!priv->is_gy_en && priv->del_mc_broadcast_filter){
+		filter_del = priv->del_mc_broadcast_filter();
+		if(filter_del)
+			pr_info("qcom-ethqos-thin: Filter delete unsuccessful");
+		else
+			pr_info("qcom-ethqos-thin: Filter delete successful");
+	}
 
 	stmmac_stop_queue(priv);
 
