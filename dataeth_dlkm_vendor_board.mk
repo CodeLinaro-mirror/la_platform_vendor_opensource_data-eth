db324c98a7a2db3e730524221be5cdfda5a06311
Srinath Pandey <quic_srinpand@quicinc.com>
2025-06-04 16:05:33 +0530

    drivers: emac_shim: Change to disable compilation
    Disable compilation for emac_shim based on product type.
    
    Change-Id: If45bcd50fd14b20762cc9c0edeeb43e332444359
    Signed-off-by: Srinath Pandey <quic_srinpand@quicinc.com>

--- dataeth_dlkm_vendor_board.mk
+++ dataeth_dlkm_vendor_board.mk
@@ -1,12 +1,18 @@
+ifneq ($(call is-board-platform-in-list, gen5), true)
+ifeq ($(TARGET_USES_GY), true)
 ifeq ($(TARGET_BOARD_PLATFORM)$(TARGET_BOARD_SUFFIX)$(TARGET_BOARD_DERIVATIVE_SUFFIX), gen4_gvm)
 ifneq ($(filter $(PLATFORM_VERSION), 16 Baklava),$(PLATFORM_VERSION))
 BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/emac_ctrl_fe_virtio.ko
 BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/emac_thin.ko
 endif
 endif
+endif
 
+ifeq ($(TARGET_USES_GY), true)
 ifeq ($(TARGET_BOARD_PLATFORM)$(TARGET_BOARD_SUFFIX)$(TARGET_BOARD_DERIVATIVE_SUFFIX), gen4_gvm_gy)
 BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/emac_thin.ko
+endif
+endif
 endif
 
 ifneq (,$(call is-board-platform-in-list2,pineapple))
