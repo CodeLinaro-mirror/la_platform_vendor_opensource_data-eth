04eeb9ad9b1a70590c75405de4e91a5ac3e16bfc
Vishal Kumar Pal <quic_vispal@quicinc.com>
2025-09-02 16:33:10 +0530

    drivers: qps615: Add fallback for PHY reset GPIO using alternate property
    If 'qcom,phy-rst-gpio' is not found in the device tree, attempt to read
    'qcom,phy-rst-gpio-id' as a fallback. This improves compatibility with
    platforms that use the alternate property name for PHY reset GPIO.
    
    Change-Id: Id7a563a1928414e3f1a7d5a9e0f1969ba4e8281d
    Signed-off-by: Vishal Kumar Pal <quic_vispal@quicinc.com>

--- drivers/qps615/src/tc956x_qcom.c
+++ drivers/qps615/src/tc956x_qcom.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 // Copyright (c) 2021, The Linux Foundation. All rights reserved.
-// Copyright (c) 2025 Qualcomm Innovation Center, Inc. All rights reserved.
+// Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
 
 #include <linux/kernel.h>
 #include <linux/device.h>
@@ -96,8 +96,11 @@ static int tc956x_platform_of_parse(struct device *dev,
 
 	if(!qpriv->has_always_on_supplies) {
 		if (of_property_read_u32(dev->of_node,"qcom,phy-rst-gpio", &qpriv->phy_rst_gpio)) {
-			dev_err(dev, "Failed to get PHY reset GPIO\n");
-			return -EINVAL;
+			if (of_property_read_u32(dev->of_node, "qcom,phy-rst-gpio-id",
+				&qpriv->phy_rst_gpio)) {
+				dev_err(dev, "Failed to get PHY reset GPIO\n");
+				return -EINVAL;
+			}
 		}
 	} else {
 		qpriv->phy_rst_gpio_som = devm_gpiod_get(dev, "phy-rst-som", GPIOD_OUT_LOW);
