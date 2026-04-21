4c587e213c9ff71eb6841ed17670d69058799b05
Jakub Lecki <jlecki@qti.qualcomm.com>
2026-04-21 11:53:18 +0200

    data-eth: Guard dataeth package/module additions with target flag.
    Change-Id: Ib372bb531933c2afdb0ea61d4cf037bb1168aa59

--- dataeth_dlkm_vendor_board.mk
+++ dataeth_dlkm_vendor_board.mk
@@ -1,4 +1,6 @@
+ifneq ($(TARGET_DISABLE_DATAETH_DLKM),true)
 ifneq ($(call is-board-platform-in-list, gen5), true)
 BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/emac_ctrl_fe_virtio.ko
 BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/emac_thin.ko
 endif
+endif # TARGET_DISABLE_DATAETH_DLKM
--- dataeth_dlkm_vendor_product.mk
+++ dataeth_dlkm_vendor_product.mk
@@ -1,3 +1,5 @@
 # Build DATA-ETH kernel drivers
+ifneq ($(TARGET_DISABLE_DATAETH_DLKM),true)
 PRODUCT_PACKAGES += $(KERNEL_MODULES_OUT)/emac_thin.ko\
 	$(KERNEL_MODULES_OUT)/emac_ctrl_fe_virtio.ko
+endif # TARGET_DISABLE_DATAETH_DLKM
